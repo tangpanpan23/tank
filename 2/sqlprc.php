0db925496c892c4cccd05838f99dcec77d68b276
tangpanpan23@126.com <tangpanpan23@126.com@db19db42-c54c-4868-b13f-452db8015102>
2014-05-28 13:54:24 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/sqlprc.php

--- 2/sqlprc.php
+++ 2/sqlprc.php
@@ -1,8 +1,8 @@
 <?php
 
 header("Content-type:text/html;charset=utf-8");
- $conn=mysql_connect("w.rdc.sae.sina.com.cn:3307", "app_tangpanpan", "tangpanpan314");
- $db = mysql_select_db("pan_pan",$conn);
+ $conn=mysql_connect("w.rdc.sae.sina.com.cn:3307", "tangpanpan23@sina.com", "tangpanpan314");
+ $db = mysql_select_db("app_tangpanpan",$conn);
  if($conn)
 	 {
 		 echo "连接mysql数据库成功!";
@@ -11,7 +11,7 @@ header("Content-type:text/html;charset=utf-8");
 		 echo "连接失败!";
 		 }
  
- $sql ="explain select * from t_mem ";
+ $sql ="explain select * from pan_pan ";
  $res = mysql_query($sql,$conn);
  $i=0;
  $arr = array();
