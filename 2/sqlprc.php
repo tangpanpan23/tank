57633e4c020eebabeca2db46318d0ca9029854d9
tangpanpan23@126.com <tangpanpan23@126.com@db19db42-c54c-4868-b13f-452db8015102>
2014-05-28 16:12:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/sqlprc.php

--- 2/sqlprc.php
+++ 2/sqlprc.php
@@ -11,7 +11,7 @@ $conn=mysql_connect("w.rdc.sae.sina.com.cn:3307", "13y1nwyj2o", "y23mihlw1hlx0kk
 		 echo "连接失败!";
 		 }
  
- $sql ="explain select * from pan_pan ";
+ $sql ="explain select * from user ";
  $res = mysql_query($sql,$conn);
  $i=0;
  $arr = array();
