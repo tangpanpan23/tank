fb36c4b8401b600cd42da957e94ac0c370b81be0
tangpanpan23@126.com <tangpanpan23@126.com@db19db42-c54c-4868-b13f-452db8015102>
2014-05-28 14:16:14 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/test.php

--- 2/test.php
+++ 2/test.php
@@ -2,11 +2,11 @@
 
 header("Content-type:text/html;charset=utf-8");
 
-echo "用户名   :".SAE_MYSQL_USER."/n";
+echo "用户名   :".SAE_MYSQL_USER."/n/r";
 
-echo "密码     :".SAE_MYSQL_PASS."/n";
+echo "密码     :".SAE_MYSQL_PASS."/r/n";
 
-echo "主库域名:".SAE_MYSQL_HOST_M."/n";
+echo "主库域名:".SAE_MYSQL_HOST_M."/r/n";
 
                echo "从库域名:".SAE_MYSQL_HOST_S;
 
