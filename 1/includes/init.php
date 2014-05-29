01cf9f311298afe8316ecf0961fde4f0d0dfdd6d
tangpanpan23@126.com <tangpanpan23@126.com@db19db42-c54c-4868-b13f-452db8015102>
2014-05-29 02:00:47 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/includes/init.php

--- 1/includes/init.php
+++ 1/includes/init.php
@@ -10,8 +10,9 @@ defined('ACC') || exit('access denied');
 // ROOT 代表网站的根路径
 define('ROOT',str_replace('\\','/',str_replace('includes\init.php','',__FILE__)));
 
-//echo "nihao";
-//exit ;
+echo "nihao";
+echo ROOT;
+exit ;
 include(ROOT . 'includes/conf.class.php');
 include(ROOT . 'includes/mysql.class.php');
 include(ROOT . 'includes/lib_base.php');
